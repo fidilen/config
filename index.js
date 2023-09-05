d262806d22f16c3ce52855fef237039b4f0b5f2d
fidilen <fidilen.dev@gmail.com>
2023-09-05 11:44:04 +0800

    Fix: constant variable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const getConfig = () => {
-    const configuration = require(process.cwd() + `/config/env.json`);
+    let configuration = require(process.cwd() + `/config/env.json`);
 
     if (configuration.ENV) {
         configuration = require(process.cwd() + `/config/env-${configuration.ENV}.json`);
